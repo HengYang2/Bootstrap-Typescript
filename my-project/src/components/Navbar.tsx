7df9b3b52db0d9f9ddc3589494e8089cb6b45d84
unknown <hengya03@gmail.com>
2023-09-13 14:59:17 -0500

    Set up file structure and Basic Navbar functionalities.

---
+++ my-project/src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import {Button, Container, Nav, Navbar as NavbarBs} from "react-bootstrap"
+import {NavLink} from "react-router-dom"
+
+export default function Navbar() {
+  return (
+    <NavbarBs className="bg-white shadow-sm mb-3"> 
+        <Container>
+            <Nav>
+                <Nav.Link to="/" as={NavLink}>
+                    Home
+                </ Nav.Link>
+                <Nav.Link to="/store" as={NavLink}>
+                    Store
+                </ Nav.Link>
+                <Nav.Link to="/about" as={NavLink}>
+                    About
+                </ Nav.Link>
+            </Nav>
+            <Button style={{width: "3rem", height: "3rem" }}>
+            </Button>
+        </Container>
+    </ NavbarBs>
+  )
+}
